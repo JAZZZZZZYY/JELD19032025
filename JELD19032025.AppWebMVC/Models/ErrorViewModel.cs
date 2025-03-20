1001fc611f8e7c077965dbe424529064ae3014f7
Jazmin Lue <MINEDUCYT@DESKTOP-GLJR0B3>
2025-03-19 22:28:12 -0600

    algunos cambios

--- JELD19032025.AppWebMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JELD19032025.AppWebMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
